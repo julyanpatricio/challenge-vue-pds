9340eb7d0de5dcb712da864fbd0028db77b5fae3
julyanpatricio <julyan.patricio@gmail.com>
2021-10-27 12:31:46 -0300

    start proyect

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="https://pdssa.com.ar/wp-content/uploads/2021/03/logo_st.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+@import '@/Var.scss';
+
+html, body, #app{
+  height: -webkit-fill-available;
+}
+
+body{
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: $background-app;
+}
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import { MdButton, MdContent, MdTabs, MdTable,MdIcon,MdField,MdMenu,MdList,MdDatepicker,MdDialog,MdCard,MdRipple } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.config.productionTip = false
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+Vue.use(MdTable)
+Vue.use(MdIcon)
+Vue.use(MdField)
+Vue.use(MdMenu)
+Vue.use(MdList)
+Vue.use(MdDatepicker)
+Vue.use(MdDialog)
+Vue.use(MdCard)
+Vue.use(MdRipple)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
